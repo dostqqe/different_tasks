d70860c2d133c43b6065b375ccc0cf5aac1b1def
dostqqe <yaroslava4548@gmail.com>
2025-05-20 13:00:47 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,8 +1,8 @@
 from random import choice
 
 
-class WordChooser():
-    def __init__(self, filename, attempts):
+class WordChooser:
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
     def choose_random_word(self) -> str:
@@ -10,19 +10,24 @@ class WordChooser():
             words = file.read().split()
         return choice(words)
 
-class NewGame():
-    def __init__(self, attempts, secret_word):
+
+class NewGame:
+    def __init__(self, attempts: int, secret_word: str) -> None:
         self.attempts = attempts
         self.secret_word = secret_word
 
     def hello(self) -> str:
-        print('Добро пожаловать в виселицу на тему "виды спорта", потому что мы за здоровый образ жизни!')
+        print(
+            'Добро пожаловать в виселицу на тему "виды спорта", '
+            'потому что мы за здоровый образ жизни!'
+        )
         print(f"У вас будет {self.attempts} попытки угадать секретное слово. Удачи!!")
-        print("Слово:", '_' * len(self.secret_word))
+        print("Слово:", "_" * len(self.secret_word))
         return self.secret_word
 
-class GetLetter():
-    def __init__(self, alphabet):
+
+class GetLetter:
+    def __init__(self, alphabet: str) -> None:
         self.alphabet = alphabet
 
     def get_letter(self) -> str:
@@ -33,8 +38,9 @@ class GetLetter():
             else:
                 return letter
 
-class PrintWord():
-    def __init__(self, secret_word, guessed_letters):
+
+class PrintWord:
+    def __init__(self, secret_word: str, guessed_letters: list) -> None:
         self.secret_word = secret_word
         self.guessed_letters = guessed_letters
 
@@ -57,7 +63,8 @@ def check_letter_in_word(secret: str, guessed: list, letter: str) -> bool:
                 return True
     return False
 
-def play_game(secret_word: str, attempts_limit: int):
+
+def play_game(secret_word: str, attempts_limit: int) -> None:
     counter = attempts_limit
     guessed = []
     word_is_guessed = ""
@@ -84,12 +91,14 @@ def play_game(secret_word: str, attempts_limit: int):
     if word_is_guessed == secret_word:
         print("Поздравляем! Вы угадали слово:", secret_word)
 
-def func():
-    chooser = WordChooser("input.txt", 3)
+
+def func() -> None:
+    chooser = WordChooser("input.txt")
     secret_word = chooser.choose_random_word()
     new_game = NewGame(3, secret_word=secret_word)
     game = new_game.hello()
     play_game(secret_word, 3)
+
 
 if __name__ == "__main__":
     func()
