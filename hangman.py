91b95d292d8210ac236584ec8951b9b2d3136894
dostqqe <yaroslava4548@gmail.com>
2025-07-29 12:18:16 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -10,7 +10,7 @@ class Main:
         self.alphabet = alphabet
 
 
-    def choose_random_word(self, player: str) -> int:
+    def choose_random_word(self, player: str) -> uuid.UUID:
         for game in self.games.values():
             if game["player"] == player and game["status"] == "active":
                 raise ValueError(f"У игрока {player} уже есть активная игра!")
@@ -25,13 +25,14 @@ class Main:
             "word": choice(words),
             "player": player,
             "attempts": self.attempts,
+            "guessed_letters": [],
             "status": "active"
         }
 
         return game_id
 
 
-    def get_game(self, game_id: int, player: str) -> dict:
+    def get_game(self, game_id: uuid.UUID, player: str) -> dict:
         game = self.games.get(game_id)
         if not game:
             raise ValueError("Игра не найдена")
@@ -40,30 +41,36 @@ class Main:
         return game
 
 
-    def attempt_to_guess(self, letter, secret_word, counter, guessed):
-        if letter not in self.alphabet:
-            return "Введите БУКВУ, пожалуйста.", guessed, counter, None
-        elif letter in guessed:
-            return "Вы уже угадали эту букву.", guessed, counter, None
-        elif letter in secret_word:
-            guessed.append(letter)
+    def attempt_to_guess(self, letter: str, game_id: uuid.UUID, player):#    letter, game_id, player
+        game = self.get_game(game_id, player)
+        if game["status"] != "active":
+            return "Игра уже завершена"
+        if letter not in game_instance.alphabet:
+            return "Введите БУКВУ"
+        if letter in game["guessed_letters"]:
+            return "Вы уже угадали эту букву"
+
+        secret_word = game["word"]
+        if letter in secret_word:
+            game["guessed_letters"].append(letter)
             positions = [i + 1 for i, l in enumerate(secret_word) if l == letter]
             message = f"Верно, буква '{letter}' на позициях: {', '.join(map(str, positions))}"
-            current_word = "".join([ch if ch in guessed else "*" for ch in secret_word])
-            return message, guessed, counter, current_word
+            current_word = "".join([ch if ch in game["guessed_letters"] else "*" for ch in secret_word])
+            if current_word == game["word"]:
+                game["status"] = "won"
+                return f"Поздравляем! Вы угадали слово: {secret_word}"
+            return f"{message}\n{current_word}"
         else:
-            counter -= 1
-            message = f"Не угадали. Количество оставшихся попыток: {counter}"
-            current_word = "".join([ch if ch in guessed else "*" for ch in secret_word])
-            return message, guessed, counter, current_word
+            game["attempts"] -= 1
+            message = f"Не угадали. Количество оставшихся попыток: {game["attempts"]}"
+            current_word = "".join([ch if ch in game["guessed_letters"] else "*" for ch in secret_word])
 
+            if game["attempts"] == 0:
+                game["status"] = "lost"
+                return f"Вы проиграли. Ответ: {game["word"]}"
+            return f"{message}\n{current_word}"
 
 def func(instance):
-    attempts = instance.attempts
-    counter = attempts
-    guessed = []
-
-
     player = input("Введите имя игрока: ").strip()
     game_id = instance.choose_random_word(player)
     secret_word = instance.games[game_id]["word"]
@@ -80,19 +87,13 @@ def func(instance):
     print(f"У вас будет {instance.attempts} попытки угадать секретное слово. Удачи!!")
     print("Слово:", "*" * len(secret_word))
 
-    word_is_guessed = "*" * len(secret_word)
-    while counter > 0 and word_is_guessed != secret_word:
+    while True:
         letter = input("Введите букву: ").lower()
-        message, guessed, counter, word_is_guessed = instance.attempt_to_guess(letter, secret_word, counter, guessed)
-        print(message)
-        print(word_is_guessed)
-
-    if word_is_guessed == secret_word:
-        print("Поздравляем! Вы угадали слово:", secret_word)
-        instance.games[game_id]["status"] = "won"
-    else:
-        print("Вы проиграли. Ответ:", secret_word)
-        instance.games[game_id]["status"] = "lost"
+        result = instance.attempt_to_guess(letter, game_id, player)
+        print(result)
+        game = instance.games[game_id]
+        if game["status"] in ("won", "lost"):
+            break
 
 
 if __name__ == "__main__":
