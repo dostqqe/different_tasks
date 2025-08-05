d718febcc9795bea566841ce163aea9a69b4fcea
dostqqe <yaroslava4548@gmail.com>
2025-08-05 14:39:50 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,27 +1,31 @@
-from random import choice
 import uuid
+from random import choice
 
 
 class Main:
-    def __init__(self, filename: str, attempts: int, alphabet: str):
+    """class."""
+
+    def __init__(self, filename: str, attempts: int, alphabet: str) -> None:
         self.filename = filename
         self.games = {}
         self.attempts = attempts
         self.alphabet = alphabet
 
     def choose_random_word(self, player: str) -> uuid.UUID:
+        """"выбирает случайное слово для игрока."""
         for game in self.games.values():
             if game["player"] == player and game["status"] == "active":
-                raise ValueError(f"У игрока {player} уже есть активная игра!")
+                error_msg = f"У игрока {player} уже есть активная игра!"
+                raise ValueError(error_msg)
 
-        with open(self.filename, "r", encoding="utf-8") as file:
+        with open(self.filename, encoding="utf-8") as file: # noqa: PTH123
             words = file.read().split()
 
         game_id = uuid.uuid4()
 
         self.games[game_id] = {
             "game_id": game_id,
-            "word": choice(words),
+            "word": choice(words), # noqa: S311
             "player": player,
             "attempts": self.attempts,
             "guessed_letters": [],
@@ -32,16 +36,20 @@ class Main:
         return game_id
 
     def get_game(self, game_id: uuid.UUID, player: str) -> dict:
+        """"получение игры."""
         game = self.games.get(game_id)
         if not game:
-            raise ValueError("Игра не найдена")
+            error_msg = "Игра не найдена"
+            raise ValueError(error_msg)
         if game["player"] != player:
-            raise ValueError("Это не ваша игра!")
+            error_msg = "Это не ваша игра!"
+            raise ValueError(error_msg)
         return game
 
-    def attempt_to_guess(
-        self, letter: str, game_id: uuid.UUID, player
-    ):  #    letter, game_id, player
+    def attempt_to_guess( # noqa: PLR0911
+        self, letter: str, game_id: uuid.UUID, player: str,
+    ) -> str: #    letter, game_id, player
+        """"попытка угадывания."""
         game = self.get_game(game_id, player)
         if game["status"] != "active":
             return "Игра уже завершена"
@@ -53,41 +61,41 @@ class Main:
         secret_word = game["word"]
         if letter in secret_word:
             game["guessed_letters"].append(letter)
-            positions = [i + 1 for i, l in enumerate(secret_word) if l == letter]
+            positions = [i + 1 for i, char in enumerate(secret_word) if char == letter]
             message = f"Верно, буква '{letter}' на позициях: {', '.join(map(str, positions))}"
             current_word = "".join(
-                [ch if ch in game["guessed_letters"] else "*" for ch in secret_word]
+                [ch if ch in game["guessed_letters"] else "*" for ch in secret_word],
             )
             if current_word == game["word"]:
                 game["status"] = "won"
                 return f"Поздравляем! Вы угадали слово: {secret_word}"
             return f"{message}\n{current_word}"
-        else:
-            game["attempts"] -= 1
-            message = f"Не угадали. Количество оставшихся попыток: {game["attempts"]}"
-            current_word = "".join(
-                [ch if ch in game["guessed_letters"] else "*" for ch in secret_word]
-            )
+        game["attempts"] -= 1
+        message = f"Не угадали. Количество оставшихся попыток: {game["attempts"]}"
+        current_word = "".join(
+            [ch if ch in game["guessed_letters"] else "*" for ch in secret_word],
+        )
 
-            if game["attempts"] == 0:
-                game["status"] = "lost"
-                return f"Вы проиграли. Ответ: {game["word"]}"
-            return f"{message}\n{current_word}"
+        if game["attempts"] == 0:
+            game["status"] = "lost"
+            return f"Вы проиграли. Ответ: {game["word"]}"
+        return f"{message}\n{current_word}"
 
 
-def func(instance):
+def func(instance: "Main") -> None:
+    """"func."""
     player = input("Введите имя игрока: ").strip()
     game_id = instance.choose_random_word(player)
     secret_word = instance.games[game_id]["word"]
     try:
-        data = instance.get_game(game_id, player)
+        instance.get_game(game_id, player)
     except ValueError as e:
         print(e)
         return
 
     print(
         'Добро пожаловать в виселицу на тему "виды спорта", '
-        "потому что мы за здоровый образ жизни!"
+        "потому что мы за здоровый образ жизни!",
     )
     print(f"У вас будет {instance.attempts} попытки угадать секретное слово. Удачи!!")
     print("Слово:", "*" * len(secret_word))
